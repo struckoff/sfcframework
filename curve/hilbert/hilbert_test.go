ca39af6ceafc99d17c9d4e0f731a882e885ce26b
Aleksei <as5423.ru@gmail.com>
2020-02-28 15:48:42 +0300

    typo

--- curve/hilbert/hilbert_test.go
+++ curve/hilbert/hilbert_test.go
@@ -95,84 +95,84 @@ func TestHilbertCurve_Decode(t *testing.T) {
 	}
 }
 
-//func TestHilbertCurve_Encode(t *testing.T) {
-//	type fields struct {
-//		dimensions uint64
-//		bits       uint64
-//	}
-//	type args struct {
-//		coords []uint64
-//	}
-//	tests := []struct {
-//		name     string
-//		fields   fields
-//		args     args
-//		wantCode uint64
-//		wantErr  bool
-//	}{
-//		//{
-//		//	"[1, 0] == 3",
-//		//	fields{
-//		//		2,
-//		//		1,
-//		//	},
-//		//	args{
-//		//		[]uint64{1, 0},
-//		//	},
-//		//	3,
-//		//	false,
-//		//},
-//		//{
-//		//	"[4, 12] == 96",
-//		//	fields{
-//		//		2,
-//		//		10,
-//		//	},
-//		//	args{
-//		//		[]uint64{4, 12},
-//		//	},
-//		//	96,
-//		//	false,
-//		//},
-//		{
-//			"[10, 34] == 1096",
-//			fields{
-//				2,
-//				10,
-//			},
-//			args{
-//				[]uint64{10, 34},
-//			},
-//			1096,
-//			false,
-//		},
-//		{
-//			"[4095, 4096, 0, 0, 0] == MaxInt64",
-//			fields{
-//				5,
-//				64,
-//			},
-//			args{
-//				[]uint64{4095, 4096, 0, 0, 0},
-//			},
-//			math.MaxInt64,
-//			false,
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c, _ := New(tt.fields.dimensions, tt.fields.bits)
-//			gotCode, err := c.Encode(tt.args.coords)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("Encode() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if gotCode != tt.wantCode {
-//				t.Errorf("Encode() gotCode = %v, want %v", gotCode, tt.wantCode)
-//			}
-//		})
-//	}
-//}
+func TestHilbertCurve_Encode(t *testing.T) {
+	type fields struct {
+		dimensions uint64
+		bits       uint64
+	}
+	type args struct {
+		coords []uint64
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		args     args
+		wantCode uint64
+		wantErr  bool
+	}{
+		//{
+		//	"[1, 0] == 3",
+		//	fields{
+		//		2,
+		//		1,
+		//	},
+		//	args{
+		//		[]uint64{1, 0},
+		//	},
+		//	3,
+		//	false,
+		//},
+		//{
+		//	"[4, 12] == 96",
+		//	fields{
+		//		2,
+		//		10,
+		//	},
+		//	args{
+		//		[]uint64{4, 12},
+		//	},
+		//	96,
+		//	false,
+		//},
+		{
+			"[10, 34] == 1096",
+			fields{
+				2,
+				10,
+			},
+			args{
+				[]uint64{10, 34},
+			},
+			1096,
+			false,
+		},
+		{
+			"[4095, 4096, 0, 0, 0] == MaxInt64",
+			fields{
+				5,
+				64,
+			},
+			args{
+				[]uint64{4095, 4096, 0, 0, 0},
+			},
+			math.MaxInt64,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := New(tt.fields.dimensions, tt.fields.bits)
+			gotCode, err := c.Encode(tt.args.coords)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Encode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotCode != tt.wantCode {
+				t.Errorf("Encode() gotCode = %v, want %v", gotCode, tt.wantCode)
+			}
+		})
+	}
+}
 
 func BenchmarkCurve_Decode(b *testing.B) {
 	log.SetOutput(ioutil.Discard)
