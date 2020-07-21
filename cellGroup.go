145f4a0726ab42a60b6b32b91b144204e744eda3
Alexey Struckov <as5423.ru@gmail.com>
2020-07-21 06:19:38 +0300

    debug

--- cellGroup.go
+++ cellGroup.go
@@ -99,7 +99,7 @@ func (cg *CellGroup) TotalLoad() (load uint64) {
 	cg.mu.Lock()
 	defer cg.mu.Unlock()
 	for iter := range cg.cells {
-		log.Println(cg.Node().ID(), cg.cells[iter].id, cg.cells[iter].id, load)
+		log.Println(cg.id, cg.cells[iter].id, cg.cells[iter].id, load)
 		load += cg.cells[iter].load
 	}
 	return
